c2506b80a1fd332960c612d3510a4e211ba1f210
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-07-09 02:51:15 +0700

    fix1

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,12 +1,6 @@
 import classNames from "classnames/bind";
 import style from "./Footer.module.scss";
 
-import style from "./Footer.module.scss";
-import classNames from "classnames/bind";
-import { baseURL } from "~/utils/Contant";
-import classNames from "classnames/bind";
-import style from "./Footer.module.scss";
-
 const cx = classNames.bind(style);
 function Footer() {
   return (
