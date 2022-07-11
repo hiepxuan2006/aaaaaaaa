59d6ddf25a2d1fb1d5764a77a5148b12350e649b
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-07-11 07:21:04 +0700

    update 1

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const httpRequest = axios.create({
-//   baseURL: "https://hx-farm.herokuapp.com/api/",
-// });
 const httpRequest = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://hx-farm.herokuapp.com/api/",
 });
+// const httpRequest = axios.create({
+//   baseURL: "http://localhost:8080/api/",
+// });
 export const get = async (path, options = {}) => {
   let response = await httpRequest.get(path, options);
   if (response.status) {
