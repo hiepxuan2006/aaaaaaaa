463ea3960d5bae5fd49e6e598c10f60b7a53e01e
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-07-13 10:20:03 +0700

    update 4

--- src/utils/Contant.js
+++ src/utils/Contant.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:8080";
-// export const baseURL = "https://hx-farm.herokuapp.com/";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://hx-farm.herokuapp.com/";
 //
 export const TOKEN_NAME = "token";
